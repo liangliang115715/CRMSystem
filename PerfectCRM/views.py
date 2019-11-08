620ed9ad56ae0c805c384bf66c91f40978a41764
ll <ll@123.com>
2019-11-08 21:28:21 +0800

    结合用户自定的认证，搞定了用户登录，注销页面的前端和后台，保证同django自带认证具有相同的功效。

--- PerfectCRM/views.py
+++ PerfectCRM/views.py
@@ -1,25 +1,42 @@
 from django.shortcuts import render,HttpResponse,redirect
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import login,logout
+from django.db.models import Q
+from CRM.models import UserProfile
+
+
+
+# 个性化验证，返回一个user对象,检测密码要用对象的check_password方法。
+def authenticate(request=None,**conditions):
+    try:
+        user = UserProfile.objects.get(Q(name=conditions['username'])|Q(email=conditions['username']))
+        if user.check_password(conditions['password']):
+            role_list = []
+            for role in user.role.values('name'):
+                role_list.append(role['name'])
+            user = user if conditions['role'] in role_list else None
+    except Exception as e:
+        print(e)
+        user = None
+    return user
 
 
 def acc_login(request):
-	error_msg=""
-	if request.method=="POST":
-		username=request.POST.get("username")
-		password=request.POST.get("password")
-		
-		user=authenticate(username=username,password=password)
-		if user:
-			login(request,user)
-			
-			return redirect(request.GET.get("next","/CRM/"))
-		else:
-			error_msg="用户名或密码错误"
-	
-	return render(request,"login.html",{"error_msg":error_msg})
+    error_msg=""
+    if request.method=="POST":
+        username=request.POST.get("username",None)
+        password=request.POST.get("password",None)
+        role = request.POST.get("role",None)
+        user=authenticate(username=username,password=password,role=role)
+        if user:
+            login(request,user)
+            return redirect(request.GET.get("next","/CRM/"))
+        else:
+            error_msg="用户名或密码错误"
+    
+    return render(request,"login.html",{"error_msg":error_msg})
 
 def acc_logout(request):
-	
-	logout(request)
-	
-	return redirect("/login/")
+    
+    logout(request)
+    
+    return redirect("/login/")
