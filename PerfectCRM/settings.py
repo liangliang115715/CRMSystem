9761da39121059474bc06b7c17045b6217347bca
ll <ll@123.com>
2020-03-03 16:36:53 +0800

    更改了验证中间件的位置

--- PerfectCRM/settings.py
+++ PerfectCRM/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'static.middleFile.PermissionMiddleware.PermissionViewMiddleware'
+    'middleFile.PermissionMiddleware.PermissionViewMiddleware',
 ]
 
 ROOT_URLCONF = 'PerfectCRM.urls'
